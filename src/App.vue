e99f93361c062d61671d67f050924f1de23c41cf
Dom Habersack <dominik.habersack@netlight.com>
2017-11-16 09:28:13 +0100

    Implemented

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <main>
+    <h1>Podcast Search</h1>
+
+    <input type="search" v-model="query" />
+
+    <section>
+      <podcast :podcast="podcast" v-for="podcast in podcasts" key="podcast.trackId" />
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Podcast from './Podcast'
+import _ from 'lodash'
 
 export default {
   name: 'app',
+  data: () => {
+    return {
+      query: '',
+      podcasts: []
+    }
+  },
   components: {
-    HelloWorld
+    'podcast': Podcast
+  },
+  watch: {
+    query: function () {
+      this.onQueryChange()
+    }
+  },
+  methods: {
+    onQueryChange: _.debounce(function () {
+      fetch(`https://itunes.apple.com/search?term=${this.query}&media=podcast`)
+          .then((response) => response.json())
+          .then((results) => {
+            this.podcasts = results.results;
+          });
+    }, 250)
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+section {
+  background: #ddd;
+  border-bottom: 0.1em solid #fff;
+  border-top: 0.1em solid #ccc;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-column-gap: 2%;
+  grid-row-gap: 1.2em;
+  padding: 1.1em 2%;
+}
+
+@media screen and (min-width: 320px) {
+  section {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media screen and (min-width: 640px) {
+  section {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media screen and (min-width: 960px) {
+  section {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+
+@media screen and (min-width: 1280px) {
+  section {
+    grid-template-columns: repeat(5, 1fr);
+  }
+}
+
+@media screen and (min-width: 1600px) {
+  section {
+    grid-template-columns: repeat(6, 1fr);
+  }
 }
 </style>
